e974c79a51dd2166831b63b9a493b13b7e7a30e2
Gitlab Exercise <tesfuad@gmail.com>
2023-10-12 18:21:00 -0600

    Adding tempreture convertor

---
+++ CelciusvsFarenheit.js
@@ -0,0 +1,10 @@
+//convert tempreture from celcius to farenheit//
+let celcius =140;
+let fahrenheit = 250;
+let celciusToFahrenheit = (celcius * 9/5) + 32;
+let fahrenheitToCelcius = (fahrenheit - 32) * 5/9;
+console.log(`farenheit to celcius: ${fahrenheitToCelcius}`);
+console.log(`celcius to farenheit: ${celciusToFahrenheit}`);
+
+
+
